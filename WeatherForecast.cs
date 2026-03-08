fe18ad03bc7ffd2859dfe6ea0c765702270c301a
balu-mmt <77828828+balu-mmt@users.noreply.github.com>
2026-03-08 22:36:19 +0530

    Update WeatherForecast.cs

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -9,6 +9,7 @@ namespace GIT_UIT
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string? Summary { get; set; 
+                               }
     }
 }
